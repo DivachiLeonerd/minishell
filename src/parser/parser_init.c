fa0fed209a3c97421dc5d646dd4addecacac1b46
Deepthought Studios <deepthoughtstudios@Clubs-iMac.local>
2023-01-07 18:30:12 +0000

    syntax error beta 0.1

---
+++ src/parser/parser_init.c
@@ -0,0 +1,33 @@
+/*************************************************************************/
+/*                                                                       */
+/*                                                                       */
+/*     HEADER FUCKING FILE FUCK YEAH MOTHERFUCKER                        */
+/*                                                                       */
+/*     Created: 69/420 who:fucking:cares by: me                          */
+/*     Updated: 69/421 who:fucking:cares by: me                          */
+/*                                                             42        */
+/*************************************************************************/
+
+t_tree *parser_init(char *s, t_list *env)
+{
+    t_tree *tree;
+    int what_is_token;
+    char *token;
+
+    while TRUE
+    {
+        token = //future get_token func
+        if (!token)
+            break;
+        if ()//token der erro
+        {
+            //free token
+            return (NULL);
+        }
+        what_is_token = get_token_type(token); //figure out what the token is
+        if (what_is_token == word)
+            token = //func to handle token if it is a werd
+        //tree building function
+    }
+    return tree
+}
