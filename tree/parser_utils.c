90296576b2b23cd136c173d95f4cc37060552f3f
BunyMan <jpbuny@gmail.com>
2023-01-30 17:30:49 +0000

    to switch branch

--- tree/parser_utils.c
+++ tree/parser_utils.c
@@ -3,15 +3,17 @@
 int get_token_type(char *token)
 {
     int token_size;
+    int token_id;
 
     token_size = (int)ft_strlen(token);
     if (!token)
         return (0);
     if (token_size ==1)
     {
-        if (token[0] == ' ')
+        if (token[0] == '>')
         {
-
+            return (1);
         }
+        
     }
 }
