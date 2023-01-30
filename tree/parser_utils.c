0dd23ed77e886677bc31663b433e6142949ff23b
BunyMan <jpbuny@gmail.com>
2023-01-30 18:05:44 +0000

    changes to parser utils

--- tree/parser_utils.c
+++ tree/parser_utils.c
@@ -8,12 +8,23 @@ int get_token_type(char *token)
     token_size = (int)ft_strlen(token);
     if (!token)
         return (0);
-    if (token_size ==1)
+    if (token_size == 1)
     {
         if (token[0] == '>')
-        {
             return (1);
-        }
-        
+        else if (token[0] == '<')
+            return (2);
+        else if (token[0] == '|')
+            return (3);
     }
+    else if (token_size == 2)
+    {
+        if (token[0] == '>' && token[1] == '>')
+            return (4);
+        else if (token[0] == '<' && token[1] == '<')
+            return (5);
+    }
+    else
+        return (6); //token in neither redir nor pipe
 }
+
