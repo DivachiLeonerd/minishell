5bb0f5aef2847b381814a5caf6b1bd2320048cb7
Afonso Fernandes <81565090+DivachiLeonerd@users.noreply.github.com>
2023-02-16 12:11:48 +0000

    norm

--- built-ins/piping.c
+++ built-ins/piping.c
@@ -6,24 +6,24 @@
 /*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:43:55 by afonso            #+#    #+#             */
-/*   Updated: 2023/02/05 17:18:19 by afonso           ###   ########.fr       */
+/*   Updated: 2023/02/16 12:11:40 by afonso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//I need a function that creates all pipes and returns the int** 
+// I need a function that creates all pipes and returns the int**
 
-int	**pipe_creation(int how_many_pipes)
+int **pipe_creation(int how_many_pipes)
 {
-	int	**pipe_fd;
+	int **pipe_fd;
 	int i;
 
 	i = 0;
 	pipe_fd = malloc(how_many_pipes * sizeof(int *));
 	while (i < how_many_pipes)
 	{
-		
+
 		pipe_fd[i] = malloc(2 * sizeof(int));
 		pipe(pipe_fd[i]);
 		i++;
@@ -31,13 +31,13 @@ int	**pipe_creation(int how_many_pipes)
 	return (pipe_fd);
 }
 
-int	output_redirection(int fd1, t_tree *node)
+int output_redirection(int fd1, t_tree *node)
 {
-	char	buf[INT32_MAX];
-	int		fd2;
-	ssize_t	readbytes;
+	char buf[INT32_MAX];
+	int fd2;
+	ssize_t readbytes;
 
-	fd2 = open(node->args[0]/*filename*/, O_CREAT, O_WRONLY);
+	fd2 = open(node->args[0] /*filename*/, O_CREAT, O_WRONLY);
 	readbytes = read(fd1, buf, INT32_MAX);
 	if (readbytes == -1)
 	{
@@ -54,18 +54,18 @@ int	piping(int *pid, int **pipe_fd, int num_of_pipes, int index)
 	if (pid[index] == 0)
 	{
 		if (index > 0)
-			dup2(pipe_fd[index - 1][0], STDIN_FILENO);//STDIN of a i process is the read part of pipe connecting it to process i - 1
+			dup2(pipe_fd[index - 1][0], STDIN_FILENO); // STDIN of a i process is the read part of pipe connecting it to process i - 1
 		if (index < num_of_pipes)
-			dup2(pipe_fd[index][1], STDOUT_FILENO);//process is writing to pipe instead of stdout
-		//I need to do error checking
+			dup2(pipe_fd[index][1], STDOUT_FILENO); // process is writing to pipe instead of stdout
+		// I need to do error checking
 	}
 	return (0);
 }
 
-t_tree	*find_command_node(int index, t_tree *bintree)
+t_tree *find_command_node(int index, t_tree *bintree)
 {
-	int 	i;
-	t_tree	*node;
+	int i;
+	t_tree *node;
 
 	i = 0;
 	while (node != bintree && i < index)
@@ -86,9 +86,9 @@ t_tree	*find_command_node(int index, t_tree *bintree)
 	return (node);
 }
 
-void	initialize_forking_processes(int *pid, int numof_processes)
+void initialize_forking_processes(int *pid, int numof_processes)
 {
-	int 	i;
+	int i;
 
 	i = 0;
 	while (i < numof_processes)
@@ -97,5 +97,5 @@ void	initialize_forking_processes(int *pid, int numof_processes)
 			perror("A problem has occured during fork process\n");
 		i++;
 	}
-	return ;
+	return;
 }
